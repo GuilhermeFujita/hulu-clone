00002d797794d446620dd9ff2fdefc1bf0b7edfc
Guilherme Fujita <fujitaguilherme@gmail.com>
2022-02-06 14:39:55 -0300

    Initial nav component and backend request creation

---
+++ components/Nav.js
@@ -0,0 +1,15 @@
+import requests from '../utls/requests'
+
+function Nav() {
+  return (
+    <nav>
+      <div className="">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2 key={key}>{title}</h2>
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <Nav />
     </div>
   )
 }
